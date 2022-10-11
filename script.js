59026b29411155b7537510407437008c9c760344
twothreetwo <tarkan.zarrouk@gmail.com>
2022-10-11 00:15:12 -0400

    project done, fuck javascript display

--- script.js
+++
@@ -1,8 +0,0 @@
-function checkEmail() {
-    const email = document.getElementsByClassName("notify-user");
-    if(!(email) || !(email.contains("@") && email.contains("."))) {
-        // remind self grab contents of error message, exclamation error thing, then create an eventListner so when it 
-        // invalid display it below
-        return;
-    }
-}
