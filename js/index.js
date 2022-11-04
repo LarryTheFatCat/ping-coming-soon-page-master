9663106c733b6f1ada316f20b9121bd6587a7b70
LarryTheFatCat@users.noreply.github.com <83801755+LarryTheFatCat@users.noreply.github.com>
2022-11-04 08:01:46 -0400

    css, js -> folder;icon{anchor};eventlistnertofunct

--- js/index.js
+++ js/index.js
@@ -2,18 +2,18 @@ const email = document.getElementById("notify");
 const button = document.getElementById("btn-notify");
 const errorMessage = document.getElementById("error-message");
 
-button.addEventListener("click", function checkEmail() {
+function checkEmail() {
   let email = document.getElementById("notify");
-  let validation
+  let validation;
   let re = /\S+@\S+\.\S+/;
 
   validation = re.test(email.value);
   console.log(validation);
 
-  if (validation === true) {
-    email.setAttribute('style', 'border: 3px solid #4f7df3;')
+  if(validation === true) {
+    email.setAttribute('style', 'border: 3px solid #4f7df3;');
   } else {
-    email.setAttribute('style', 'border: 3px solid #ff5263;')
-    res.innerHTML = "please enter a valid email address!"
+    email.setAttribute('style', 'border: 3px solid #ff5263;');
+    res.innerHTML = "please enter a valid email address!";
+  }
 }
-});
