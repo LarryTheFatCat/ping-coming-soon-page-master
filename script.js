4fdfa2f5e0f1a3625942d046a656b8c9057cc5a6
LarryTheFatCat@users.noreply.github.com <83801755+LarryTheFatCat@users.noreply.github.com>
2022-11-03 22:31:39 -0400

    set html tag in css to body.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const email = document.getElementById("notify")[0].value;
 const button = document.getElementById("btn-notify")[0].value;
+const errorMessage = document.getElementById("error-message")[0].value;
 
-function checkEmail() {}
+function checkEmail() {
+    if(!email || !email.includes("@") || !email.includes(".")) {
+        errorMessage.style.display = "block";
+    } else {
+        errorMessage.style.display = "none";
+    }
+}
