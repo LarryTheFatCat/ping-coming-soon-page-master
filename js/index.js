d2bd49739c7d80728fb536ff3efd07b521471d36
LarryTheFatCat@users.noreply.github.com <83801755+LarryTheFatCat@users.noreply.github.com>
2022-12-18 23:54:24 -0500

    restart, fuck sass

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const email = document.getElementById("notify");
+const button = document.getElementById("btn-notify");
+const errorMessage = document.getElementById("error-message");
+
+function checkEmail() {
+  let email = document.getElementById("notify");
+  let validation;
+  let re = /\S+@\S+\.\S+/;
+
+  validation = re.test(email.value);
+  console.log(validation);
+
+  if(validation === true) {
+    email.setAttribute('style', 'border: 3px solid #4f7df3;');
+  } else {
+    email.setAttribute('style', 'border: 3px solid #ff5263;');
+    res.innerHTML = "please enter a valid email address!";
+  }
+}
